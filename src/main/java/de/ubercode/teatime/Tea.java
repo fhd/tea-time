66c3296e53c878a71ac5c50c16e90cd45019804a
Felix H. Dahlke <fhd@ubercode.de>
2010-09-10 23:36:13 +0200

    Fixed POM and code style.

--- src/main/java/de/ubercode/teatime/Tea.java
+++ src/main/java/de/ubercode/teatime/Tea.java
@@ -1,43 +1,35 @@
 package de.ubercode.teatime;
 
-public class Tea
-{
+public class Tea {
 	private String name;
 	private int drawTime;
 
-	public Tea()
-	{
+	public Tea() {
 	}
 
-	public Tea(String name, int drawTime)
-	{
+	public Tea(String name, int drawTime) {
 		this.name = name;
 		this.drawTime = drawTime;
 	}
 
-	public String getName()
-	{
+	public String getName()	{
 		return this.name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public int getDrawTime()
-	{
+	public int getDrawTime() {
 		return this.drawTime;
 	}
 
-	public void setDrawTime(int drawTime)
-	{
+	public void setDrawTime(int drawTime) {
 		this.drawTime = drawTime;
 	}
 
-	public String getLabel()
-	{
+	public String getLabel() {
 		return this.name + ((this.drawTime > 0)
-				? " [" + String.valueOf(this.drawTime) + "s]" : "");
+                            ? " [" + String.valueOf(this.drawTime) + "s]" : "");
 	}
 }
